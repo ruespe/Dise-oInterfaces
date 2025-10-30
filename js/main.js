9edc2ac60ae1a3589417b037e774287f5a5c8285
ruespe <rubenespin84@gmail.com>
2025-10-30 16:40:32 +0100

    mejorillas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-document.querySelector("#modo")
+//import './scss/main.scss';
+
+
+document.querySelector("#modos").addEventListener("change", function() {
+    if (this.value === "dark") {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.add("light-mode");
+    }
+});
