e2c8cfb2143bdff6e8c33717981e148b0d33c74e
ruespe <rubenespin84@gmail.com>
2025-11-01 20:12:25 +0100

    localStorage hecho

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 import "boxicons/css/boxicons.min.css";
 
 let botonModo = document.querySelector(".cambiarModo");
+let modoGuardado = localStorage.getItem("modo")
+
+if(modoGuardado){
+  document.body.classList.add(modoGuardado)
+}else{
+  document.body.classList.add("light-mode")
+}
 
 botonModo.addEventListener("click", () => {
   if (document.body.classList.contains("light-mode")) {
-    document.body.classList.remove("light-mode");
-    document.body.classList.add("dark-mode");
+    document.body.classList.replace("light-mode", "dark-mode");
+    localStorage.setItem("modo", "dark-mode")
   } else{
-    document.body.classList.remove("dark-mode")
-    document.body.classList.add("light-mode")
+    document.body.classList.replace("dark-mode", "light-mode");
+    localStorage.setItem("modo", "light-mode")
   }
 });
